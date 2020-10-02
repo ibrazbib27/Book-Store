65030d3f6567a342ce8a4eeb1492c5f4cfbbc272
Ibrahim Zbib <abraham.zbib@gmail.com>
2020-10-02 11:19:26 -0400

    navbar and frontend switch set up as well as scss complete

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,27 +1,44 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import BooksNavbar from "./components/books_navbar/BooksNavbar";
 
-const App = (props: AppProps) => {
-	const [greeting, setGreeting] = React.useState<string>('');
-
-	React.useEffect(() => {
-		(async () => {
-			try {
-				const res = await fetch('/api/sup');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
+export interface bookType{
+id: number;
+title: string;
+author: string;
+price: number;
+name: string;
+categoryid: number;
+}
+interface AppProps {}
+const App: React.FC<AppProps> = (props) => {
 
 	return (
-		<div className="min-vh-100 d-flex justify-content-center align-items-center">
-			<h1 className="display-1">Sup {greeting}!</h1>
-		</div>
+		<Router>
+			<Switch>
+				{['/'].map((path) => (
+					<Route
+						path={path}
+						exact
+						key={path}
+						render={(props) => (
+							<>
+								<BooksNavbar  history={props.history} location={props.location} match={props.match} />
+								<Container fluid className={'d-flex align-items-center justify-content-center bg-color min-vh-100'}>
+									<p className={'display-2'}>Welcome!</p>
+								</Container>
+							</>
+						)}
+					/>
+				))}
+			</Switch>
+		</Router>
 	);
 };
 
-interface AppProps {}
+
 
 export default App;
